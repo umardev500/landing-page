c8ff7e37b0fb2f651b6ebec2bbc5b93bfa1360ee
umar <umar.dev500@gmail.com>
2023-06-04 10:11:48 +0700

    add aos to hero

--- src/components/organisms/hero/Hero.tsx
+++ src/components/organisms/hero/Hero.tsx
@@ -4,12 +4,12 @@ export const Hero: React.FC = () => {
   return (
     <section className="px-4">
       <div className="container mx-auto grid py-10 lg:grid-cols-3">
-        <div className="col-span-2 flex items-center">
+        <div data-aos="fade-right" className="col-span-2 flex items-center">
           <h1 className="text-6xl leading-tight text-gray-700 lg:pr-10">
             Rediscover Your Strength and Resilience through the Power of Therapy
           </h1>
         </div>
-        <div className="flex justify-center">
+        <div data-aos="fade-left" data-aos-delay={100} className="flex justify-center">
           <picture>
             <img src="image/cover.png" alt="cover" />
           </picture>
