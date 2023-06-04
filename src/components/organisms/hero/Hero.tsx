408ba34b8eeb4a840efb7a78c2019293f2c85417
umar <umar.dev500@gmail.com>
2023-06-04 12:59:19 +0700

    add update

--- src/components/organisms/hero/Hero.tsx
+++ src/components/organisms/hero/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Hero: React.FC = () => {
   return (
-    <section className="px-4">
+    <section className="px-4 pb-10">
       <div className="container mx-auto grid py-10 lg:grid-cols-3">
         <div data-aos="fade-right" className="col-span-2 flex items-center">
           <h1 className="text-6xl leading-tight text-gray-700 lg:pr-10">
